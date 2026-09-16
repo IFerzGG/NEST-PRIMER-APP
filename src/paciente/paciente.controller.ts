51be8680f6f82ddd7d660e066f11807a8852f9b5
IFerzGG <ferchomuser@icloud.com>
2026-09-15 20:13:35 -0600

    feat: CRUD completo de pacientes y medicos con decoradores de NestJS

--- src/paciente/paciente.controller.ts
+++ src/paciente/paciente.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { PacienteService } from './paciente.service.js';
 
 @Controller('paciente')
@@ -8,5 +8,25 @@ export class PacienteController {
     @Get()
     findAll(){
         return this.pacienteService.findAll();
+    }
+
+    @Get(":id")
+    findId(@Param("id") id:string){
+        return this.pacienteService.findId(+id);
+    }
+
+    @Post()
+    create(@Body() data:{nombre:string, fechaNacimiento:Date, email:string, telefono:string}){
+        return this.pacienteService.create(data);
+    }
+
+    @Patch(":id")
+    update(@Param("id") id:string, @Body() data:{nombre?:string, fechaNacimiento?:Date, email?:string, telefono?:string}){
+        return this.pacienteService.update(+id, data);
+    }
+
+    @Delete(":id")
+    remove(@Param("id") id:string){
+        return this.pacienteService.remove(+id);
     }
 }
--- src/paciente/paciente.service.ts
+++ src/paciente/paciente.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service.js';
 
 @Injectable()
@@ -10,5 +10,46 @@ export class PacienteService {
             orderBy: {id:"asc"}
         });
         return resultado;
+    }
+
+    async findId(id:number){
+        const resultado = await this.prisma.paciente.findUnique({
+            where:{id}
+        });
+        if(!resultado){
+            throw new NotFoundException("Paciente no encontrado");
+        }
+        return resultado;
+    }
+
+    async create(data:{nombre:string, fechaNacimiento:Date, email:string, telefono:string}){
+        return await this.prisma.paciente.create({
+            data,
+        });
+    }
+
+    async update(id:number, data:{nombre?:string, fechaNacimiento?:Date, email?:string, telefono?:string}){
+        const encontrado = await this.prisma.paciente.findUnique({
+            where:{id},
+        });
+        if(!encontrado){
+            throw new NotFoundException("Paciente no encontrado");
+        }
+        return await this.prisma.paciente.update({
+            where:{id},
+            data,
+        });
+    }
+
+    async remove(id:number){
+        const encontrado = await this.prisma.paciente.findUnique({
+            where:{id},
+        });
+        if(!encontrado){
+            throw new NotFoundException("Paciente no encontrado");
+        }
+        return await this.prisma.paciente.delete({
+            where:{id},
+        });
     }
 }
